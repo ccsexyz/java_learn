d8b47dbd4f0e5504a6a268f37c5807bdf5bd6720
ccsexyz <ccsexyz@hotmail.com>
2015-10-01 16:08:29 +0800

    add CalendarTest

---
+++ test/test.java
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: test.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年10月01日 星期四 11时27分37秒
+ ************************************************************************/
+
+import java.util.*;
+
+public class test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("We will not use 'Hello World!'");
+        System.out.println(new Date());
+    }
+}
